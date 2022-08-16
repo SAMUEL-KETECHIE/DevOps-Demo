068d76a31215b5944145283f7480d0e365b03ec6
Samuel_Wendolin <samuelwendolin2@gmail.com>
2022-08-16 05:07:54 +0000

    chore(project-init): initialized repo with Demo Project and Dockerfile

---
+++ 3_coding/ssl-certificate-checker/Abstractions/ICertificateHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SslCertificateChecker.Abstractions
+{
+    public interface ICertificateHelper
+    {
+        Task<string> CheckDomainSSLExpiry(string domain);
+    }
+}
